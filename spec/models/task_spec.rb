1c9777096865579b3236471fd43e46b33de818cb
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-27 22:38:57 -0700

    Remove/add whitespace to pretty up and better organize test data and method 'ends'

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Task, type: :model do
   describe "creation" do 
     it "is instantiated as an instance of the Task class with title and description attributes" do 
       task1 = Task.create!(title: "Yardwork", description: "Mow lawn, pick up dog droppings")
-
       expect(task1.class).to be(Task)
       expect(task1.title).to eq("Yardwork")
       expect(task1.description).to eq("Mow lawn, pick up dog droppings")
     end
   end
+
 end 

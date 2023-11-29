404adcec5598345ed466f046e7f091b2f628e7dc
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-28 17:19:16 -0700

    New seed data files to browser test site functionality using rails server

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Task.destroy_all
+
+task1 = Task.create!(title: "Yardwork", description: "Mow lawn, pick up dog droppings")
+task2 = Task.create!(title: "Grocery Shopping", description: "Go to Costco and King Soopers")
+task3 = Task.create!(title: "Laundry", description: "Fold clothes on dryer")

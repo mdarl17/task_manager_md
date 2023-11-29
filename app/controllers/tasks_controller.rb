ebb8743efe00d970ca937ea7e27f00845fcf3b43
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-29 15:20:06 -0700

    Define #new and #create methods in tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,19 @@ class TasksController < ApplicationController
   def show 
     @task = Task.find(params[:id])
   end
+
+  def new 
+
+  end
+
+  def create 
+    new_task = Task.new({title: params[:title], description: params[:description]})
+    if new_task.save
+      flash[:alert] = "Task successfully added!"
+      redirect_to "/tasks"
+    else 
+      flash[:alert] = "Uh-oh. Something went wrong and the task was not added. Please try again."
+      redirect_to "/tasks/new"
+    end
+  end
 end
